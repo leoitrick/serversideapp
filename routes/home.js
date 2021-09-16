f45e3442492bfb212227c1540b179eaf123d095a
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-09-15 23:33:07 -0400

    confg for HJS  templates

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("this is from the router");
+  res.render("home", null);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const path = require("path");
 const home = require("./routes/home");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hjs");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", home);
 
 app.listen(5000);
