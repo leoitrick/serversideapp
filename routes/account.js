30023b9f32a318792757bc6ee0aa752d324cf3db
Leandro Moreira <leoitrick@Leandros-MacBook-Pro.local>
2021-09-19 20:50:43 -0400

    Added account template

--- routes/account.js
+++ routes/account.js
@@ -2,17 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
+const items = [
+  { name: "item 1", description: "", price: 10 },
+  { name: "item 2", description: "", price: 20 },
+  { name: "item 3", description: "", price: 15 },
+  { name: "item 4", description: "", price: 30 },
+  { name: "item 5", description: "", price: 40 },
+];
+
 router.get("/", (req, res, next) => {
-  res.json({
-    user: req.user || "Not Logged in",
-  });
+  const user = req.user;
+  if (user == null) {
+    res.redirect("/");
+    return;
+  }
+
+  const data = {
+    user: user,
+  };
+
+  res.render("account", data);
 });
 
 router.get("/logout", (req, res, next) => {
   req.logout();
-  res.json({
-    confirmation: "User Logged out!",
-  });
+  res.redirect("/");
 });
 
 module.exports = router;
